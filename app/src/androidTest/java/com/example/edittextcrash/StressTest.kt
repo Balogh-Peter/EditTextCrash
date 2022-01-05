d39e172609cff921ed0c63042b137f5d7276ec3a
Peter Balogh <balogh.peter@bape.dev>
2022-01-05 16:44:42 +0100

    Removed temporary debug code

--- app/src/androidTest/java/com/example/edittextcrash/StressTest.kt
+++ app/src/androidTest/java/com/example/edittextcrash/StressTest.kt
@@ -10,12 +10,7 @@ class StressTest {
     @Test
     fun stressTest() {
         for (i in 1..500) {
-            val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-            activityScenario.onActivity {
-                it.findViewById<AppCompatEditText>(R.id.editText).requestFocus()
-                it.findViewById<AppCompatEditText>(R.id.editText).clearFocus()
-                Thread.sleep(500)
-            }
+            ActivityScenario.launch(MainActivity::class.java)
             Log.d("StressTest", "Launched activity number:  $i")
         }
     }
